9e6dfb245de37874f2dd5cea5d81bf55c8f62971
Wellspring Church <wellspringchurch@wellsprings-imac.local>
2018-07-16 09:58:24 -0400

    Initial Commit

---
+++ Wellspring Church/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wellspring Church
+//
+//  Created by Wellspring Church on 7/16/18.
+//  Copyright © 2018 Wellspring Church. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
